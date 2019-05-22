48ed5f005feb6b7f9a4eff25e61a386e1be1fea7
Fawaz <Mba7eth@gmail.com>
2019-05-22 02:53:16 -0700

    Update BST.py

--- algorithms/BST.py
+++ algorithms/BST.py
@@ -8,61 +8,71 @@ class Node(object):
 	def __repr__(self):
 		return str(self.val)
 		
-class Tree(object):
-	def __init__(self, root = None):
-		self.root = root		
+class BSTree(object):
+	def __init__(self):
+		self.root = None		
 	def setRoot(self, root):
 		self.root = root
 	def add(self, val):
 		if not self.root:
-			self.setRoot(node)
+			self.setRoot(Node(val))
 			return 
 		if val in self:
-			print(val, "already exists in the Tree.")
 			return 
 		parent = None
 		cur = self.root	
 		while cur:
-			parent = cur 
-			cur = cur.left if val > cur.val else cur.right
-		if parent.val > val:
+			parent = cur  
+			cur = cur.left if val < cur.val else cur.right
+		if val < parent.val:
 			parent.left = Node(val)
 		else:
-			parent.right = node
-
+			parent.right = Node(val)	
 	def __contains__(self, val):
 		return self.contains(val)
 	def contains(self, val):
 		if not self.root:
 			return False 
+		cur = self.root
 		while cur:
-			if cur.val == node.val:
-				return True 
-			cur = cur.left if val > cur.val else cur.right
+			if cur.val == val: 
+				return True
+			cur = cur.left if val < cur.val else cur.right
 		return False 
 	def remove(val):
 		pass
-	def inorder(self):
-		stack = [self.root]
+
+	def inOrder(self):
+		stack = []
 		cur = self.root
 		inOrder = []
-		while stack:
-			if not cur: 
+		while True:
+			if cur:
 				stack.append(cur)
 				cur = cur.left 
 			elif stack:
-				cur = stack.pop() 
+				cur = stack.pop()  
 				inOrder.append(cur.val)
 				cur = cur.right
 			else:
 				break
-		return inOrder  
-				
-			
-	
-		
-					
-		
-		
-		
+		return inOrder
+	def postOrder(self):
+			stack = []
+			cur = self.root
+			inOrder = []
+			while True:
+				if cur:
+					stack.append(cur)
+					cur = cur.left 
+				elif stack:
+					cur = stack.pop()  
+					inOrder.append(cur.val)
+					cur = cur.right
+				else:
+					break
+			return inOrder
+
+
+
 		

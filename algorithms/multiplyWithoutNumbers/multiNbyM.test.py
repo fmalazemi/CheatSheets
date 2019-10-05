1de897b5e8efc84e755c305f331f062a744b5994
Fawaz <Mba7eth@gmail.com>
2019-10-05 22:29:24 +0300

    Create multiNbyM.test.py

---
+++ algorithms/multiplyWithoutNumbers/multiNbyM.test.py
@@ -0,0 +1,12 @@
+from multiNbyM import multiNbyN
+
+
+import random
+for i in range(100):
+    x = random.randint(0,1000)
+    y = random.randint(0, 1000)
+
+    if(x*y !=  int(multiNbyN(str(x), str(y)))):
+        print(x*y, multiNbyN(str(x), str(y)))
+    else:
+        print("Passed")
